7e1bd3e9d30496fde8c24f84afa210090b872dcb
AlvieTheGreat <alvareskenito@gmail.com>
2023-10-24 12:37:07 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: zero always
+  */
+
+int main(void)
+{
+	int counter = 2;
+	long int x = 1;
+	long int y = x + 1;
+	long int z = x + y;
+
+	printf("%ld, %ld, ", x, y);
+	while (counter < 50)
+	{
+		printf("%ld", z);
+		counter++;
+		x = y;
+		y = z;
+		z = x + y;
+		if (counter < 50)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
