6130c88e63c5e8914e8e6493f00d0a4723f85b19
AlvieTheGreat <alvareskenito@gmail.com>
2023-11-08 06:13:36 -0800

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,34 +14,25 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *new_ptr;
 	unsigned int i;
 
-	/* If new size is 0, it's equivalent to free(ptr) */
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	/* If ptr is NULL, equivalent to malloc(new_size) */
 	if (ptr == NULL)
 	{
 		return (malloc(new_size));
 	}
-
-	/* If new size is the same as old size, no action needed */
 	if (new_size == old_size)
 	{
 		return (ptr);
 	}
-
-	/* Allocate memory for the new block */
 	new_ptr = malloc(new_size);
 
 	if (new_ptr == NULL)
 	{
 		return (NULL);
 	}
-
-	/* Copy old data to new block */
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
@@ -56,8 +47,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			*((char *)new_ptr + i) = *((char *)ptr + i);
 		}
 	}
-
-	/* Free the original block */
 	free(ptr);
 
 	return (new_ptr);
